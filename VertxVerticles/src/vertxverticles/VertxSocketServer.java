252fb8686570e791a759a6f2e42c75a4c1a789d5
unknown <niklas.alander@outlook.com>
2017-12-11 21:15:54 +0100

    vertx servers + DB + canvas

--- VertxVerticles/src/vertxverticles/VertxSocketServer.java
+++ VertxVerticles/src/vertxverticles/VertxSocketServer.java
@@ -11,6 +11,7 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.ServerWebSocket;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.streams.Pump;
 
 /**
@@ -23,26 +24,12 @@ public class VertxSocketServer extends AbstractVerticle {
         System.out.println("starting VertxSocketServer");
         HttpServer server = vertx.createHttpServer();
 
-        /*server.requestHandler(new Handler<HttpServerRequest>() {
-            public void handle(HttpServerRequest req) {
-                req.bodyHandler(event -> {
-                   req.response().end(event.toString());
-                });
-            }
-        }).listen(8083, "localhost");*/
-        
         server.websocketHandler(new Handler<ServerWebSocket>() {
             public void handle(ServerWebSocket ws) {
                 System.out.println("new connection to VertxSocketServer");
-                // A WebSocket has connected!
-                //Pump.pump(ws, ws).start();
-                //ws.write(Buffer.buffer("from java server"));
                 
                 ws.handler(event -> {
-                    //System.out.println(event.toString());
-                    // when something is sent to this particular socket
-                    //ws.write(Buffer.buffer("new messages received.. " + event.toString()));
-                    ws.writeTextMessage(event.toString());
+                    ws.write(Buffer.buffer(event.toString()));
                 });
             }
         }).listen(8082, "localhost");
